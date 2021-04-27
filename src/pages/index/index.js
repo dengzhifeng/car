4c378eb618e4c0ed512e42112080cb7206b5887a
dengzhifeng <398577351@qq.com>
2021-04-27 17:20:01 +0800

    feat:2021-04-27-17:19:09

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,31 +1,45 @@
-import React, { Component } from 'react'
-import { View } from '@tarojs/components'
+/*
+ * @description:
+ * @author: steve.deng
+ * @Date: 2021-04-21 17:34:36
+ * @LastEditors: steve.deng
+ * @LastEditTime: 2021-04-25 17:49:16
+ */
+import React, { Component } from "react";
+import { View } from "@tarojs/components";
 
-import Header from '../../containers/Header/Header'
-import MainSection from '../../containers/MainSection/MainSection'
+import Header from "../../containers/Header/Header";
+import MainSection from "../../containers/MainSection/MainSection";
 
-import './index.scss'
+import "./index.scss";
 
 class Index extends Component {
-
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
+    let date = new Date();
+
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours();
+    let min = date.getMinutes();
     return (
-      <View className='todaoapp'>
+      <View className="todaoapp">
+        <View>version 1.3.0</View>
+        {month}月{day}日{hour}时{min}分
         <Header />
         <MainSection />
       </View>
-    )
+    );
   }
 }
 
-export default Index
+export default Index;
